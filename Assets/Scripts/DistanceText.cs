7078bc0bec1f1a6ccef16f717749db643e889582
ialy1595 <ialy12006@gmail.com>
2019-04-10 23:16:56 +0900

    init project

---
+++ Assets/Scripts/DistanceText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    private Text dist;
+    public float distPerTime = 10;
+    
+    void Start()
+    {
+        dist = GetComponent<Text>();
+        dist.text = "0m";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist.text = "" + (int)(GameManager.gm.gameTime * distPerTime) + "m";
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
+using System.ComponentModel;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+
+    public GameObject Gamemanager;
+    public static GameObject _Gamemanager;
+    private static bool GMCreated = false;
+
+    private bool isGameStart = false;
+    
+    [HideInInspector] public bool isPaused;
+
+    [HideInInspector] public float gameTime;
+    private float gameStartTime;
+    private float gamePauseTime;
+    private float pauseStartTime;
+    private bool pauseStart;
+
+
+    void Awake()
+    {
+        if (GMCreated == true)  // GM 중복생성 방지
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Application.targetFrameRate = 60;
+        DontDestroyOnLoad(this);    // 씬이 넘어가도 파괴되지 않음
+
+        gm = this;
+        _Gamemanager = Gamemanager;
+    }
+    
+    void Start()
+    {
+        if (GMCreated == true)  // GM 중복생성 방지
+            return;
+        GMCreated = true;
+
+        Random.InitState((int)Time.time);
+    }
+
+    void Update()
+    {
+        if(isGameStart)
+        {
+            TimeUpdate();
+        }
+    }
+
+    public void GameStartInit()
+    {
+        isPaused = false;
+        gameTime = 0;
+        gamePauseTime = 0;
+        gameStartTime = Time.time;
+        isGameStart = true;        
+    }
+
+    void GameEnd()
+    {
+
+    }
+
+    void TimeUpdate()
+    {
+        gameTime = Time.time - gameStartTime - gamePauseTime;
+    }
+}
